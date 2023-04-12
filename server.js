72aeacb6b83bdb3e9d093dcd1246b040d3e4d481
William Combee <william.combee@yahoo.com>
2023-04-12 14:07:51 -0500

    added consts,get paths,post path

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs')
+const notes = require('./db/db.json');
+
+// Specify on which port the Express.js server will run
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//  Static middleware pointing to the public folder
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Create Express.js routes for default '/', '/send' and '/routes' endpoints
+
+// Get /notes path
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+//Post notes path
+app.post('/api/notes', (req, res) => {
+    req.body.id = notes.length += 1;
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        let currentNotes = JSON.parse(data);
+        currentNotes.push(req.body);
+        fs.writeFile('./db/db.json', JSON.stringify(currentNotes), (err) => {
+            if (err) {
+                console.log(err);
+            }
+        })
+    });
+    res.json();
+})
+
+ // listen() method is responsible for listening for incoming connections on the specified port 
+app.listen(PORT, () => {
+    console.log(`app listening at http://localhost:${PORT} `);
+});
